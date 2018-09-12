6103e92447eda65b4ad52a962c004cbbaa12d16c
Kwangwook-Kwon <kwangwook.kwon@gmail.com>
2018-09-12 14:12:33 +0900

    test

--- Sample_program/main.cc
+++ Sample_program/main.cc
@@ -2,16 +2,20 @@
 #include "ranvar.h"
 #include "ranvar.cc"
 
+#define NUMBER_OF_FLOWS 1000
+#define INPUT_CDF_FILE "workload_search.tcl"
+
 int main(){
+
     EmpiricalRandomVariable* flow = new  EmpiricalRandomVariable;
-    flow->loadCDF("workload_search.tcl");
-    char* flow_text;
+    flow->loadCDF(INPUT_CDF_FILE);
+    char* flow_text[NUMBER_OF_FLOWS];
 
-    for(int i=0; i<1; i++)
-
-        flow_text=flow->flow_text();
-        printf("%s\n", flow_text);
-        printf("%d\n", (int) strlen(flow_text));
-
+    for(int i=0; i<NUMBER_OF_FLOWS; i++)
+    {
+        flow_text[i]=flow->flow_text();
+        printf("%d\n", (int) strlen(flow_text[i]));
+        //printf("%s\n", flow_text[i]);
+    }
     return 0;
 }
